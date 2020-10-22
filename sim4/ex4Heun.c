191f67c199c46602bd48f9e37beeba05b7247671
yudai0731 <yudai40018@gmail.com>
2020-10-22 11:26:42 +0900

    4回目の進捗

--- sim4/ex4Heun.c
+++ sim4/ex4Heun.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define MAX_STEP 2
+#define MAX_STEP 1
 
 double func(double x,double y){
-    return y/(1+x);
+    return 2*y/(1+x);
 }
 
-double Euler(double x0,double y0,double h){
+double Heun(double x0,double y0,double h){
     double x=x0,y=y0;
-    double xp,yp;
+    double xp,yp,k1,k2;
     for(int i=0;i<MAX_STEP;i++){
         xp = x+h;
-        yp = y+h*func(x,y);
+        k1 = h*func(x,y);
+        k2 = h*func(x+h,y+k1);
+        yp = y+(k1+k2)/2;
         printf("t=%d   x=%lf,y=%lf\n",i+1,xp,yp);
         x=xp;
         y=yp;
@@ -20,10 +22,11 @@ double Euler(double x0,double y0,double h){
 
 int main(int argc,char *argv[]){
     double x0 = 0;
-    double y0 = 1;
+    double y0 = 0.5;
     double h = 0.5;
     printf("-----setting-----\n");
     printf("h=%lf\nt=0   x=%lf,y=%lf\n",h,x0,y0);
-    Euler(x0,y0,h);
+    Heun(x0,y0,h);
+    
     return 0;
 }
--- sim4/kadai4.c
+++ sim4/kadai4.c
@@ -12,19 +12,21 @@ double Euler(double x0,double y0,double h){
     for(int i=0;i<MAX_STEP;i++){
         xp = x+h;
         yp = y+h*func(x,y);
-        printf("t=%d   x=%lf,y=%lf\n",i+1,xp,yp);
+        printf("t=%02d   x=%lf,y=%lf\n",i+1,xp,yp);
         x=xp;
         y=yp;
     }
 }
 
-double EulerCsv(double x0,double y0,double h){
+double Heun(double x0,double y0,double h){
     double x=x0,y=y0;
-    double xp,yp;
+    double xp,yp,k1,k2;
     for(int i=0;i<MAX_STEP;i++){
         xp = x+h;
-        yp = y+h*func(x,y);
-        printf("%d,%lf,%lf\n",i+1,xp,yp);
+        k1 = h*func(x,y);
+        k2 = h*func(x+h,y+k1);
+        yp = y+(k1+k2)/2;
+        printf("t=%02d   x=%lf,y=%lf\n",i+1,xp,yp);
         x=xp;
         y=yp;
     }
@@ -34,15 +36,11 @@ int main(int argc,char *argv[]){
     double x0 = 0;
     double y0 = 1;
     double h = 0.1;
-    /* format for stdout 
     printf("-----setting-----\n");
-    printf("h=%lf\nt=0   x=%lf,y=%lf\n",h,x0,y0);
+    printf("h=%lf\nt=00   x=%lf,y=%lf\n",h,x0,y0);
+    printf("\n-----Euler method-----\n");
     Euler(x0,y0,h);
-    */
-
-    /* format for csv */
-    printf("0,%lf,%lf\n",x0,y0);
-    EulerCsv(x0,y0,h);
-
+    printf("\n-----Heun method-----\n");
+    Heun(x0,y0,h);
     return 0;
 }
